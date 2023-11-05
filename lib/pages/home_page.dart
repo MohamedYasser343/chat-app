2c4a0f5d7144132bc2db9e939f68a71686cd0a34
Mohamed Yasser <96927790+MohamedYasser343@users.noreply.github.com>
2023-11-05 12:11:44 +0200

    Add files via upload

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,96 @@
+import 'package:chat_app/pages/chat_page.dart';
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sing user out
+  void signOut() {
+    // get auth service
+    final authService = Provider.of<AuthService>(
+      context,
+      listen: false,
+    );
+    authService.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        actions: [
+          IconButton(
+            onPressed: signOut,
+            icon: const Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
+      ),
+      body: _buildUserList(),
+    );
+  }
+
+  // build a list of users except the current one
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  // build individual user item list
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    // display all user except the current one
+    if (_auth.currentUser!.email != data['email']) {
+      return ListTile(
+        title: Text(
+          data['email'],
+        ),
+        onTap: () {
+          // pass the clicked user's ID to the chat page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiveruserEmail: data['email'],
+                receiverUserID: data['uid'],
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      // return black container
+      return Container();
+    }
+  }
+}
---
+++ lib/services/auth/login_or_register.dart
@@ -0,0 +1,31 @@
+import 'package:chat_app/pages/login_page.dart';
+import 'package:chat_app/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+  // initally show login page
+  bool showloginPage = true;
+
+  // toggle between login and register pages
+  void togglePages() {
+    setState(() {
+      showloginPage = !showloginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showloginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
